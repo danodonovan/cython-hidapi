b611eca28c0d7212db8098a723ca51e9cc6dfe38
Dan O'Donovan <dan.odonovan@gmail.com>
2014-01-26 18:33:10 +0000

    py3k compatibility

--- try.py
+++ try.py
@@ -1,21 +1,21 @@
+from __future__ import print_function
 import hid
 import time
 
 for d in hid.enumerate(0, 0):
     keys = d.keys()
-    keys.sort()
-    for key in keys:
-        print "%s : %s" % (key, d[key])
-    print ""
+    for key in sorted(keys):
+        print("%s : %s" % (key, d[key]))
+    print("")
 
 try:
-    print "Opening device"
+    print("Opening device")
     h = hid.device(0x461, 0x20)
     #h = hid.device(0x1941, 0x8021) # Fine Offset USB Weather Station
 
-    print "Manufacturer: %s" % h.get_manufacturer_string()
-    print "Product: %s" % h.get_product_string()
-    print "Serial No: %s" % h.get_serial_number_string()
+    print("Manufacturer: %s" % h.get_manufacturer_string())
+    print("Product: %s" % h.get_product_string())
+    print("Serial No: %s" % h.get_serial_number_string())
 
     # try non-blocking mode by uncommenting the next line
     #h.set_nonblocking(1)
@@ -27,18 +27,18 @@ try:
                 h.write([0x80, i, j])
                 d = h.read(5)
                 if d:
-                    print d
+                    print(d)
                 time.sleep(0.05)
 
-    print "Closing device"
+    print("Closing device")
     h.close()
 
-except IOError, ex:
-    print ex
-    print "You probably don't have the hard coded test hid. Update the hid.device line"
-    print "in this script with one from the enumeration list output above and try again."
+except IOError as ex:
+    print(ex)
+    print("You probably don't have the hard coded test hid. Update the hid.device line")
+    print("in this script with one from the enumeration list output above and try again.")
 
-print "Done"
+print("Done")
 
 
 
